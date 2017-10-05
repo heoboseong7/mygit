41f6b604d21c60e377d1d03fc5bb33b6fedf56f3
BoSeong <boseong844@gmail.com>
2017-10-05 22:23:16 +0900

    D번 성공

--- 2017CPC/D번/소스.cpp
+++ 2017CPC/D번/소스.cpp
@@ -2,30 +2,48 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 int compare(const void *a, const void *b)
 {
-	if (*(double*)a > *(double*)b)
+	if ((*(double*)a) > (*(double*)b))
 		return 1;
-	else if(*(double*)a < *(double*)b)
-		return -1;
-	else
+	else if ((*(double*)a) == (*(double*)b))
 		return 0;
+	else
+		return -1;
+
 }
 
+int findmax(double *a)
+{
+	int max = 0;
+	for (int i = 1; i < 7; i++)
+		if (a[max] < a[i])
+			max = i;
+
+	return max;
+}
 int main()
 {
 	int n;
 	int i;
 	scanf("%d", &n);
-	double *score = (double*)malloc(sizeof(double) * n);
-	for (i = 0; i < n; i++)
-		scanf("%lf", score + i);
 
-	qsort(score, n, sizeof(double), compare);
+	double theseven[7];
+	double score;
+	for (i = 0; i < 7; i++)
+		scanf("%lf", theseven + i);
+
+	for (i = 7; i < n; i++)
+	{
+		scanf("%lf", &score);
+		if (theseven[findmax(theseven)] > score)
+			theseven[findmax(theseven)] = score;
+	}
+
+	qsort(theseven, 7, sizeof(double), compare);
 
 	for (i = 0; i < 7; i++)
-		printf("%.3lf\n", score[i]);
+		printf("%.3lf\n", theseven[i]);
 
 	return 0;
 }
